ad0d01e11a7f4eb387f55b6979c4eb3bb6de568e
Timur <kulkaevta@mail.ru>
2023-04-08 22:35:02 +0300

    Added comments and decorators

--- bot.py
+++ bot.py
@@ -11,43 +11,50 @@ from support_chat_handler import support_chat_handler
 
 
 def check_user_state(message, state):
-    return message.chat.id in user_context_state and user_context_state[message.chat.id] == state
-
+    return (
+        message.chat.id in user_context_state
+        and user_context_state[message.chat.id] == state
+    )
 
 
 # Enum State Bot: Start = 1
 # Обработчик команд /start и /help
-bot.message_handler(commands=['start', 'help'])(start_help_handler)
+bot.message_handler(commands=["start", "help"]
+)(start_help_handler)
 
 
 # Enum State Bot: Registration = 2
 # Обработчик сообщений в состоянии регистрации
-bot.message_handler(func=lambda message: check_user_state(message, StateBot.Registration))
-(registration_handler_state)
+bot.message_handler(
+    func=lambda message: check_user_state(message, StateBot.Registration)
+)(registration_handler_state)
 
 # Enum State Bot: Profile = 3
 # Обработчик команды /profile
-bot.message_handler(commands=['profile'])
-(show_profile_handler)
+bot.message_handler(commands=["profile"]
+)(show_profile_handler)
 
 # Enum State Bot: PhonePriceSearch = 4
 # Обработчик сообщений
-bot.message_handler(func=lambda message: check_user_state(message, StateBot.PhonePriceSearch))
-(find_phone_price_handler)
+bot.message_handler(
+    func=lambda message: check_user_state(message, StateBot.PhonePriceSearch)
+)(find_phone_price_handler)
 
 # Enum State Bot: SupportChat = 5
 # Обработчик сообщений
-bot.message_handler(func=lambda message: check_user_state(message, StateBot.SupportChat))
-(support_chat_handler)
+bot.message_handler(
+    func=lambda message: check_user_state(message, StateBot.SupportChat)
+)(support_chat_handler)
 
 
 # Обработчик callback-запросов (для других целей)
-bot.callback_query_handler(func=lambda call: True)(callback_handler)
+bot.callback_query_handler(func=lambda call: True
+)(callback_handler)
 
 # Эхо-обработчик для всех остальных сообщений
-bot.message_handler(func=lambda message: True)(echo_all_handler)
+bot.message_handler(func=lambda message: True
+)(echo_all_handler)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     bot.polling(none_stop=True)
